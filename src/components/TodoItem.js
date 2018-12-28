5e728e5f0c620eea2ed109e25f894d2fed50ac48
Brian Bycynski <high5bri@hotmail.com>
2018-12-28 05:35:28 -0500

    finished

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 
 export default class TodoItem extends Component {
   render() {
+    const deleteButton = {
+      cursor: 'pointer'
+    };
+
+    const { title, handleDelete, handleEdit } = this.props;
     return (
       <li className="list-group-item text-capitalize d-flex justify-content-between mt-2">
-        <h6>title</h6>
+        <h6>{title}</h6>
         <div className="todo-icon">
-          <span className="mx-2 text-success">
+          <span
+            style={deleteButton}
+            className="mx-2 text-success"
+            onClick={handleEdit}
+          >
             <i className="fas fa-pen" />
           </span>
-          <span className="mx-2 text-danger">
+          <span
+            style={deleteButton}
+            className="mx-2 text-danger"
+            onClick={handleDelete}
+          >
             <i className="fas fa-trash" />
           </span>
         </div>
